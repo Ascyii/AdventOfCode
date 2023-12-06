0873eb5fd921b21eaad93e1742e0d0864b4bd365
Ascyii <jonashahn1@gmx.net>
2023-12-06 12:32:31 +0100

    5 own

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,154 +1,55 @@
-use std::str::FromStr;
+use std::str::Split;
 
 advent_of_code::solution!(5);
 
-#[derive(Debug, PartialEq)]
-struct AlmanacMapEntry {
-    dest_start: u64,
-    source_start: u64,
-    length: u64,
-}
-
-impl AlmanacMapEntry {
-    fn convert(&self, source: u64) -> Option<u64> {
-        if self.source_start <= source && source < (self.source_start + self.length) {
-            Some(source - self.source_start + self.dest_start)
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-struct AlmanacMap(Vec<AlmanacMapEntry>);
-
-#[derive(Debug, PartialEq)]
-struct ValueRange {
-    start: u64,
-    length: u64,
-}
-
-impl AlmanacMap {
-    fn convert(&self, source: u64) -> u64 {
-        match self
-            .0
-            .iter()
-            .map(|entry| entry.convert(source))
-            .find_map(|e| e)
-        {
-            Some(dest) => dest,
-            None => source,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-struct Almanac {
-    seeds: Vec<u64>,
-    maps: Vec<AlmanacMap>,
-}
-
-impl Almanac {
-    fn seed_to_location(&self, seed: u64) -> u64 {
-        self.maps.iter().fold(seed, |value, map| map.convert(value))
-    }
-}
-
-#[derive(Debug, PartialEq)]
-struct ParseAlmanacError;
-
-impl FromStr for Almanac {
-    type Err = ParseAlmanacError;
-
-    fn from_str(text: &str) -> Result<Self, Self::Err> {
-        let mut seeds = Vec::new();
-        let mut maps = Vec::new();
-
-        for (ix, section) in text.split("\n\n").enumerate() {
-            if ix == 0 {
-                if let Some(seeds_str) = section.strip_prefix("seeds: ") {
-                    for seed in seeds_str.split_whitespace().map(u64::from_str) {
-                        let seed = seed.map_err(|_| ParseAlmanacError)?;
-                        seeds.push(seed);
+fn parse_mappings(input: Split<&str>) -> Vec<Vec<(u64, u64, u64)>> {
+    let mut maps: Vec<Vec<(u64, u64, u64)>> = Vec::new();
+    for map_input in input {
+        let mut map:Vec<(u64, u64, u64)> = Vec::new();
+        let lines = map_input.split('\n').skip(1);
+        for line in lines {
+            let mut line = line.split(' ');
+            if let Ok(destination) = line.next().unwrap().parse::<u64>() {
+                if let Ok(source) = line.next().unwrap().parse::<u64>() {
+                    if let Ok(length) = line.next().unwrap().parse::<u64>() {
+                        map.push((destination, source, length));
                     }
-                } else {
-                    return Err(ParseAlmanacError);
                 }
-            } else {
-                let map: AlmanacMap = section.parse()?;
-                maps.push(map);
             }
         }
-
-        Ok(Self { seeds, maps })
+        maps.push(map);
     }
+    maps
 }
 
-impl FromStr for AlmanacMap {
-    type Err = ParseAlmanacError;
-
-    fn from_str(text: &str) -> Result<Self, Self::Err> {
-        let mut entries = Vec::new();
-
-        for (ix, line) in text.lines().enumerate() {
-            if ix == 0 {
-                continue;
-            }
-
-            let entry: AlmanacMapEntry = line.parse()?;
-            entries.push(entry);
-        }
-
-        Ok(Self(entries))
-    }
-}
-
-impl FromStr for AlmanacMapEntry {
-    type Err = ParseAlmanacError;
-
-    fn from_str(text: &str) -> Result<Self, Self::Err> {
-        let mut dest_start: Result<u64, Self::Err> = Err(ParseAlmanacError);
-        let mut source_start: Result<u64, Self::Err> = Err(ParseAlmanacError);
-        let mut length: Result<u64, Self::Err> = Err(ParseAlmanacError);
-
-        for (ix, value) in text
-            .split_whitespace()
-            .map(|value| u64::from_str(value).map_err(|_| ParseAlmanacError))
-            .enumerate()
-        {
-            match ix {
-                0 => dest_start = value,
-                1 => source_start = value,
-                2 => length = value,
-                _ => return Err(ParseAlmanacError),
+fn walk_mappings(seed: u64, mappings: &Vec<Vec<(u64, u64, u64)>>) -> u64 {
+    let mut current = seed;
+    for map in mappings {
+        'map: for line in map {
+            let (destination, source, lenght) = line;
+            if current >= *source && current < (source + lenght) {
+                let offset = current - source;
+                current = destination + offset;
+                break 'map;
             }
         }
-
-        let dest_start = dest_start?;
-        let source_start = source_start?;
-        let length = length?;
-
-        Ok(AlmanacMapEntry {
-            dest_start,
-            source_start,
-            length,
-        })
     }
+    current
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    if let Ok(almanac) = input.parse::<Almanac>() {
-        almanac
-            .seeds
-            .iter()
-            .map(|seed| almanac.seed_to_location(*seed))
-            .min()
-    } else {
-        None
+    let mut input = input.split("\n\n");
+    let mut results: Vec<u64> = Vec::new();
+    let seeds = input.next().unwrap().split(' ').skip(1);
+    let mappings = parse_mappings(input);
+    for seed in seeds {
+        let seed: u64 = seed.parse::<u64>().unwrap();
+        results.push(walk_mappings(seed, &mappings));
     }
+    Some(*results.iter().min().unwrap())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -4,7 +4,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
