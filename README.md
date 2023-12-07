8e64ef9a57d0c4741c08929b78689e7f122b23ad
Ascyii <jonashahn1@gmx.net>
2023-12-07 12:39:51 +0100

    day 7

--- README.md
+++ README.md
@@ -22,13 +22,13 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `235.6µs` | `825.4µs` |
-| [Day 2](./src/bin/02.rs) | `50.0µs` | `70.5µs` |
-| [Day 3](./src/bin/03.rs) | `4.5ms` | `3.6ms` |
-| [Day 4](./src/bin/04.rs) | `1.5ms` | `1.5ms` |
-| [Day 5](./src/bin/05.rs) | `25.0µs` | `40.2µs` |
-| [Day 6](./src/bin/06.rs) | `341.0ns` | `21.5ms` |
-| [Day 7](./src/bin/07.rs) | `-` | `-` |
+| [Day 1](./src/bin/01.rs) | `236.4µs` | `839.0µs` |
+| [Day 2](./src/bin/02.rs) | `49.0µs` | `67.1µs` |
+| [Day 3](./src/bin/03.rs) | `4.5ms` | `3.8ms` |
+| [Day 4](./src/bin/04.rs) | `382.1µs` | `384.8µs` |
+| [Day 5](./src/bin/05.rs) | `21.5µs` | `41.1µs` |
+| [Day 6](./src/bin/06.rs) | `340.0ns` | `21.0ms` |
+| [Day 7](./src/bin/07.rs) | `6.9ms` | `7.0ms` |
 
-**Total: 33.85ms**
+**Total: 45.22ms**
 <!--- benchmarking table --->
--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,11 +1,199 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+
 advent_of_code::solution!(7);
 
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+// Card to hold value and make it comparable
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Card(u8);
+impl From<char> for Card {
+    fn from(value: char) -> Self {
+        let mut digit: u8 = 15;
+        if value.is_numeric() {
+            digit = value.to_digit(10).unwrap() as u8;
+        } else if value == 'T' {
+            digit = 10;
+        } else if value == 'J' {
+            digit = 11;
+        } else if value == 'Q' {
+            digit = 12;
+        } else if value == 'K' {
+            digit = 13;
+        } else if value == 'A' {
+            digit = 14;
+        }
+        if digit < 15 {
+            Card(digit)
+        } else {
+            panic!()
+        }
+    }
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+// Hand as container for cards
+struct Hand {
+    cards: [Card; 5],
+}
+impl Hand {
+    fn create(raw: &str, jokers: bool) -> Self {
+        let cards_raw = raw.chars();
+        let mut cards = [Card(0); 5];
+        for (card, value) in cards.iter_mut().zip(cards_raw) {
+            let mut value_new = value;
+            if jokers && value == 'J' {
+                value_new = '1';
+            }
+            *card = Card::from(value_new);
+        }
+        Hand { cards }
+    }
+    // Evaluate hand for poker scoring 1-7
+    fn evaluate(&self, jokers: bool) -> u8 {
+        let mut dups: Vec<u8> = Vec::new();
+        let mut joker: u8 = 0;
+        if jokers {
+            joker = self
+                .cards
+                .iter()
+                .filter(|&&x| x == Card(1))
+                .count()
+                .try_into()
+                .unwrap();
+        }
+        for card in self.get_unique(jokers) {
+            let dup: u8 = self
+                .cards
+                .iter()
+                .filter(|&&x| x == card)
+                .count()
+                .try_into()
+                .unwrap();
+            dups.push(dup);
+        }
+        // Apply joker rule
+        if joker != 0 {
+            dups.sort();
+            let mut dups_amount = dups.len();
+            if dups_amount == 0 {
+                dups_amount += 1;
+                dups.push(0);
+            }
+            dups[dups_amount - 1] += joker;
+        }
+        // Check type of hand
+        if dups.contains(&5) {
+            return 7;
+        }
+        if dups.contains(&4) {
+            return 6;
+        }
+        if dups.contains(&3) && dups.contains(&2) {
+            return 5;
+        }
+        if dups.contains(&3) {
+            return 4;
+        }
+        if dups.iter().filter(|&&x| x == 2).count() == 2 {
+            return 3;
+        }
+        if dups.iter().filter(|&&x| x == 2).count() == 1 {
+            return 2;
+        }
+        1
+    }
+    fn get_unique(&self, jokers: bool) -> Vec<Card> {
+        // Convert the vector to a HashSet to remove duplicates
+        let mut unique: HashSet<_> = self.cards.into_iter().collect();
+        if jokers {
+            unique.remove(&Card(1));
+        }
+        unique.into_iter().collect()
+    }
+    // Get identity to compare Hands from right to left
+    fn get_identity(&self) -> u128 {
+        let mut identity = String::new();
+        for card in self.cards {
+            let mut card_str = card.0.to_string();
+            if card_str.len() == 1 {
+                card_str.insert(0, '0');
+            }
+            identity += &card_str;
+        }
+        identity.parse::<u128>().unwrap()
+    }
+}
+
+// Players struct contains hand and bidding value
+struct Player {
+    hand: Hand,
+    value: u8,
+    bid: u32,
+}
+impl Player {
+    fn create(value: &str, jokers: bool) -> Self {
+        let mut input = value.split(' ');
+        // Parse fields from input
+        let hand: Hand = Hand::create(input.next().unwrap(), jokers);
+        let bid: u32 = input.next().unwrap().parse::<u32>().unwrap();
+        let value: u8 = hand.evaluate(jokers);
+        // Init player with rank of null
+        Player { hand, value, bid }
+    }
+}
+
+// Runs the game logic
+struct Game {
+    players: Vec<Player>,
+}
+impl Game {
+    fn create(input: &str, jokers: bool) -> Self {
+        let input = input.split('\n');
+        let mut players: Vec<Player> = Vec::new();
+        for player_arg in input {
+            let player = Player::create(player_arg, jokers);
+            players.push(player);
+        }
+        Game { players }
+    }
+    fn evaluate(&self) -> u32 {
+        // Group players into hand values
+        let mut player_groups: Vec<Vec<&Player>> = Vec::new();
+        for score in (1..=7).rev() {
+            player_groups.push(
+                self.players
+                    .iter()
+                    .filter(|&player| player.value == score)
+                    .collect_vec(),
+            );
+        }
+        // Set starting rank
+        let mut rank: u32 = self.players.len().try_into().unwrap();
+        let mut result: u32 = 0;
+        for group in player_groups.iter_mut() {
+            // Sort players in group by their highcard
+            group.sort_by(|a, b| b.hand.get_identity().cmp(&a.hand.get_identity()));
+            for player in group {
+                // Add multiply of rank and value to result
+                result += player.bid * rank;
+                // Next player is one rank down
+                rank -= 1;
+            }
+        }
+        result
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    // Create game from str
+    let game = Game::create(input, false);
+    // Evaluate the games value
+    Some(game.evaluate())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    // Run with joker flag
+    let game = Game::create(input, true);
+    Some(game.evaluate())
 }
 
 #[cfg(test)]
@@ -14,13 +202,19 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        // Normalize the input String
+        let input = &input.replace("\r\n", "\n");
+        let result = part_one(input);
+        assert_eq!(result, Some(6440));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        // Normalize the input String
+        let input = &input.replace("\r\n", "\n");
+        let result = part_two(input);
+        assert_eq!(result, Some(5905));
     }
 }
