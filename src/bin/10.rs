b17f863fe79a18238e343cfeda19d93e20fc67ed
Ascyii <jonashahn1@gmx.net>
2023-12-15 18:10:53 +0100

    first from laptop

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -12,23 +12,55 @@ pub fn part_one(input: &str) -> Option<i32> {
                 let mut directions: Option<[(i32, i32); 2]> = Some([(0, 0), (0, 0)]);
                 match value {
                     '.' => directions = None,
-                    'J' => directions = Some([(col_index as i32 - 1, row_index as i32), (col_index as i32, row_index as i32 + 1)]),
-                    'L' => directions = Some([(col_index as i32, row_index as i32 - 1), (col_index as i32 + 1, row_index as i32)]),
-                    '|' => directions = Some([(col_index as i32, row_index as i32 - 1), (col_index as i32, row_index as i32 + 1)]),
-                    '-' => directions = Some([(col_index as i32 - 1, row_index as i32), (col_index as i32 + 1, row_index as i32)]),
-                    'F' => directions = Some([(col_index as i32, row_index as i32 + 1), (col_index as i32 + 1, row_index as i32)]),
-                    '7' => directions = Some([(col_index as i32 - 1, row_index as i32), (col_index as i32, row_index as i32 + 1)]),
-                    'S' => directions = {
-                        start = ((col_index, row_index), [(0, 0), (0, 0), (0, 0), (0, 0)]); 
-                        None
-                    },
+                    'J' => {
+                        directions = Some([
+                            (col_index as i32 - 1, row_index as i32),
+                            (col_index as i32, row_index as i32 + 1),
+                        ])
+                    }
+                    'L' => {
+                        directions = Some([
+                            (col_index as i32, row_index as i32 - 1),
+                            (col_index as i32 + 1, row_index as i32),
+                        ])
+                    }
+                    '|' => {
+                        directions = Some([
+                            (col_index as i32, row_index as i32 - 1),
+                            (col_index as i32, row_index as i32 + 1),
+                        ])
+                    }
+                    '-' => {
+                        directions = Some([
+                            (col_index as i32 - 1, row_index as i32),
+                            (col_index as i32 + 1, row_index as i32),
+                        ])
+                    }
+                    'F' => {
+                        directions = Some([
+                            (col_index as i32, row_index as i32 + 1),
+                            (col_index as i32 + 1, row_index as i32),
+                        ])
+                    }
+                    '7' => {
+                        directions = Some([
+                            (col_index as i32 - 1, row_index as i32),
+                            (col_index as i32, row_index as i32 + 1),
+                        ])
+                    }
+                    'S' => {
+                        directions = {
+                            start = ((col_index, row_index), [(0, 0), (0, 0), (0, 0), (0, 0)]);
+                            None
+                        }
+                    }
                     _ => panic!(),
                 }
                 ((row_index, col_index), directions)
             })
         })
         .collect();
-    dbg!(&mappings);
+     dbg!(&mappings);
     None
 }
 
